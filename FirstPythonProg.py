e86168a3fa6a3b8e1352d7553d1f6e05b6bfff66
srisgithub <sri.singaluri@gmail.com>
2018-03-24 13:40:39 +0000

    First file to Git

---
+++ FirstPythonProg.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python   # Shebang  # Optional
+
+'Single Line & Multi-Line Comments'
+
+"Single Line :"
+
+#1. ''
+#2. ""
+#3. ''' '''
+#4. """ """
+#5. #
+
+'''Multi-Line :'''
+
+#1. ''' '''
+#2. """ """
+
+""" Creating Variables in Python """
+Fname = 'Guido'						# String data type
+_mname = "Van"						# String data type
+lName = '''Rossum'''				# String data type
+VerSion = """Python 3.6.0"""  		# String data type
+currentStableVersion = 3.6		# Float data type
+Demo_Date = 20171026				# Int data type
+
+"Accessing the Variables in python"
+
+print(Fname)
+print("")
+print(_mname)
+print("")
+print(lName)
+print("")
+print(VerSion)
+print("")
+print(currentStableVersion)
+print("")
+print(Demo_Date)
+
